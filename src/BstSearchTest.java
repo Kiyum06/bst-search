26d0838bea06c26bbf24565d9d5a60c53084401b
Your Name <kiyum1406@gmail.com>
2026-09-23 11:58:56 -0700

    added a test of target is in  left subtree

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -3,4 +3,13 @@ import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
 
+    @Test
+    void containsValueInLeftSubtree() {
+        BinaryTreeNode<Integer> root =
+            new BinaryTreeNode<>(10,
+                new BinaryTreeNode<>(5),
+                new BinaryTreeNode<>(15));
+
+        assertTrue(BstSearch.contains(root, 5));
+    }
 }
